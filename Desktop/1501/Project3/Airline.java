20f5422e94bbbf25301529ad8f2d2169f6fa38b4
Alex Bush <alexandrabush1@gmail.com>
2015-03-08 15:57:10 -0400

    driver program. doesn't do anything yet.

---
+++ Desktop/1501/Project3/Airline.java
@@ -0,0 +1,17 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Airline
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		String file = s.nextLine();
+
+		System.out.println("Filename: " + file);
+
+
+	}
+}	
